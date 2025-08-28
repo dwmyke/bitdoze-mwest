b19ec2ce70518b94042bf5a83eab291f7a447dae
Mikel-Jon West <dwmyke@gmail.com>
2025-08-28 15:40:39 -0500

    removed authors, replaced SEO with site config info

--- src/content/config.ts
+++ src/content/config.ts
@@ -10,32 +10,12 @@ const postsCollection = defineCollection({
       description: z.string().optional(),
       date: z.date().optional(),
       image: image(),
-      authors: z.array(z.string()).default(["admin"]),
       categories: z.array(z.string()).default(["others"]),
       tags: z.array(z.string()).default(["others"]),
       series: z.tuple([z.string(), z.string()]).optional(),
       canonical: z.string().optional(),
       draft: z.boolean().optional(),
     }),
-});
-
-// Author collection schema
-const authorsCollection = defineCollection({
-  schema: z.object({
-    id: z.string().optional(),
-    title: z.string(),
-    meta_title: z.string().optional(),
-    image: z.string().optional(),
-    description: z.string().optional(),
-    social: z
-      .object({
-        facebook: z.string().optional(),
-        twitter: z.string().optional(),
-        instagram: z.string().optional(),
-      })
-      .optional(),
-    draft: z.boolean().optional(),
-  }),
 });
 
 // Pages collection schema
@@ -76,6 +56,5 @@ const aboutCollection = defineCollection({
 export const collections = {
   posts: postsCollection,
   pages: pagesCollection,
-  authors: authorsCollection,
   about: aboutCollection,
 };
